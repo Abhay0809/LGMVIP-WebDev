ecc1c4541232f2f1200c4d221268ddc5b231bd74
Abhay0809 <abhayanandkk23@gmail.com>
2021-10-13 14:40:04 +0530

    task-3

---
+++ task-3/Readme.md
@@ -0,0 +1,49 @@
+# Student Result Management System
+
+This project was built for LetsGrowMore Virtual Internship program Task 3. 
+
+### Student Result Management System divided in two modules–
+
+- Student
+- Admin
+
+## Admin Features
+
+- Admin Dashboard
+- Admin can add/update/ Class
+- Admin can add/update/ Subjects
+- Admin can add/update/ Active/Inactive Subject combination with class
+- Admin can register new student and also edit info of the student
+- Admin can declare/ edit  result of a student.
+- Admin can change own password
+
+## Students Feature
+
+- Student can search their result using valid roll number.
+
+## How to run this Project
+
+1. Install the XAMPP
+2. Put all files in the folder inside the root directory(htdocs)
+
+## Database Configuration
+
+- Open phpmyadmin (http://localhost/phpmyadmin)
+- Create Database srms
+- Import database srms.sql (db/srms.sql)
+
+## Starting Server
+
+For User
+
+- Open Your browser put inside browser `http://localhost/<FOLDER_NAME>`
+
+For Admin Panel
+
+- Open Your browser put inside browser `http://localhost/<FOLDER_NAME>`
+
+Login Details for admin :
+- Username: admin 
+- Password: admin
+
+
---
+++ task-3/includes/leftbar.php
@@ -0,0 +1,61 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/VInfinite017/phpo1DxRH.jpeg" alt="Abhay Anand" class="img-circle profile-img" width="100" height="100">
+                                <h6 class="title">Abhay Anand</h6>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Home</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Accessiblity</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Classes Data</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Create Class</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Manage Classes</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Subjects Data</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students Data </span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Results Data</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Add Result</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Manage Result</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span>Change Password</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
